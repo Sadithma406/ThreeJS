aac8744235b064a349b9e230af63482bc1a72286
SadithmaPerera <sadithma.20242193@iit.ac.lk>
2026-09-14 17:06:47 +0530

    adding a text on 3js

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
-import * as THREE from 'three';
+import * as THREE from "three";
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
+import {FontLoader} from "three/addons/loaders/FontLoader.js";
+import {TextGeometry} from "three/addons/geometries/TextGeometry.js";
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const scene = new THREE.Scene();
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+const controls = new OrbitControls(camera, renderer.domElement);
+const fontLoader = new FontLoader();
+fontLoader.load(
+  'https://cdn.jsdelivr.net/npm/three@0.149.0/examples/fonts/helvetiker_regular.typeface.json',
+  (font) => {
+    const textGeo = new TextGeometry('Hello World', {
+      font: font,
+      size: 0.5,
+      height: 0.5,
+      curveSegments: 12,
+    });
+
+    textGeo.center(); 
+    const textMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+    const textMesh = new THREE.Mesh(textGeo, textMaterial);
+    textMesh.position.y = 2;
+    scene.add(textMesh);
+  });
 camera.position.z = 8;
 
 function animate(time) {
